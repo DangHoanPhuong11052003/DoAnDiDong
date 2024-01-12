1810b6ba247976df755baed419c9fe2213f8cb94
Vongola <mr.boss.vongola@gmail.com>
2024-01-12 09:37:24 +0700

    --change

--- app_adidark_store/lib/main.dart
+++ app_adidark_store/lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:app_adidark_store/Screens/ChiTietHoaDon/ChiTietHoaDon_Screen.dart';
-import 'package:app_adidark_store/Screens/Gio_Hang/CartScreen.dart';
 import 'package:app_adidark_store/Screens/HoaDon/HoaDon_Screen.dart';
-import 'package:app_adidark_store/Screens/SignUp_In/SignInScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'Screens/ChiTiet_SanPham/ProDetailScreen.dart';
+import 'Screens/SignUp_In/SignInScreen.dart';
 import 'firebase_options.dart';
 import 'Provider/google_sign_in.dart';
 
@@ -22,7 +20,7 @@ class MyApp extends StatelessWidget {
       create: (context) => GoogleSignInProvider(),
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(fontFamily: "Mulish"),
+        theme: ThemeData(primaryColor: Colors.blue, fontFamily: "Mulish"),
         home: const HoaDon_Screen(),
         // sdt: 123
         // pas: abc
