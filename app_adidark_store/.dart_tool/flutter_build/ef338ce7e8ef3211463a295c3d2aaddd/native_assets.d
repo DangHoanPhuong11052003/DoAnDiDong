7eee3d505fde4f2b8bbb7234cc47aa57800cb3ea
NATUREMILKTEA <0306191236@caothang.edu.vn>
2024-01-12 13:17:02 +0700

    Update gì đó

---
+++ app_adidark_store/.dart_tool/flutter_build/ef338ce7e8ef3211463a295c3d2aaddd/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\D\\DoAnDiDong_1\\app_adidark_store\\.dart_tool\\flutter_build\\ef338ce7e8ef3211463a295c3d2aaddd\\native_assets.yaml: 
---
+++ app_adidark_store/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\D\\DoAnDiDong_1\\app_adidark_store" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\D\\DoAnDiDong_1\\app_adidark_store"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\D\\DoAnDiDong_1\\app_adidark_store\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\D\\DoAnDiDong_1\\app_adidark_store"
+  "FLUTTER_TARGET=D:\\D\\DoAnDiDong_1\\app_adidark_store\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zZjNlNTYwMjM2NTM5YjdlMjcwMmY1YWM3OTBiMmE0NjkxYjMyZDQ5Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\D\\DoAnDiDong_1\\app_adidark_store\\.dart_tool\\package_config.json"
+)
