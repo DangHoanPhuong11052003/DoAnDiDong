f1100161711cbb66d12ed95d405217b8f699d5e0
Đặng Hoàn Phương <146814457+DangHoanPhuong@users.noreply.github.com>
2024-01-14 23:38:38 +0700

    Thêm một số class li quan đến sản phẩm, và hàm lấy dữ liệu sản phẩm

--- app_adidark_store/lib/main.dart
+++ app_adidark_store/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_adidark_store/items/BottomMenu.dart';
 import 'package:app_adidark_store/views/ChiTietHoaDon/ChiTietHoaDon_Screen.dart';
+import 'package:app_adidark_store/views/ChiTiet_SanPham/ProDetailScreen.dart';
 import 'package:app_adidark_store/views/Gio_Hang/CartScreen.dart';
 import 'package:app_adidark_store/views/SignUp_In/SignInScreen.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primaryColor: Colors.blue, fontFamily: "Mulish"),
-      home: const CartScreen(),
+      home: const ProDetailScreen(),
       // email: abc@gmail.com
       // pass: 123
       debugShowCheckedModeBanner: false,
