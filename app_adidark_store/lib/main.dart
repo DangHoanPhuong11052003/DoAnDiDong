4de40fe9e195724adeea091f27cf14dd9fb356ae
Đặng Hoàn Phương <146814457+DangHoanPhuong@users.noreply.github.com>
2024-01-20 14:36:32 +0700

    Revert "Merge branch 'main' into phuong"
    This reverts commit 439c6bb936c75530cd32755b370da10d25ec7ac8, reversing
    changes made to 05cadcf76f48b1f41670f8987a66e0cb9d08e9f8.

--- app_adidark_store/lib/main.dart
+++ app_adidark_store/lib/main.dart
@@ -1,30 +1,28 @@
 import 'package:app_adidark_store/items/auth_page.dart';
-import 'package:app_adidark_store/views/SignUp_In/SignInScreen.dart';
-import 'package:app_adidark_store/views/SignUp_In/respository/Authentication_Repository.dart';
-import 'package:app_adidark_store/views/ThongBao_CaiDat_CaNhan/profile_screen.dart';
-import 'package:app_adidark_store/views/TrangChu/HomePage.dart';
+import 'package:app_adidark_store/views/ChiTiet_SanPham/ProDetailScreen.dart';
+import 'package:app_adidark_store/views/Gio_Hang/CartScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:get/get.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-      .then((value) => Get.put(Auth_Resposity()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget  {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primaryColor: Colors.blue, fontFamily: "Mulish"),
-      home: const ProfileScreen(),
+      home: const CartScreen(),
+      // email: tự đăng ký
+      // pass: tự đăng -up
       debugShowCheckedModeBanner: false,
     );
   }
